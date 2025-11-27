4ef03bdaf05987565d1ec52b41eebae9dfe18290
Frank Feng <61307655+FrankFeng-23@users.noreply.github.com>
2025-11-27 14:43:12 +0000

    Update shapefile path to a placeholder

--- tessera_preprocessing/convert_shp_to_tiff.py
+++ tessera_preprocessing/convert_shp_to_tiff.py
@@ -261,7 +261,7 @@ def main():
     Main function to run the conversion process.
     """
     # Input shapefile path
-    shp_path = '/home/zf281/rds/rds-airr-p3-w8D3JcRiKZQ/cambridge/cambridge_shp/cambridge.shp'
+    shp_path = 'absolute_path_to_your_shp_file'
 
     # Call the conversion function
     try:
